0c62b998f3dcb57e5fa5bb02de76553bf06c47a5
Muhammad Farid <142093385+muhammadfariddd@users.noreply.github.com>
2023-10-06 03:46:31 +0700

    Input Program
    Input program materials on campus

---
+++ Latihan Input.py
@@ -0,0 +1,24 @@
+print ("***************************************************************")
+print ("****************** Program Inputan Biodata ********************")
+print ("***************************************************************")
+
+print (" ")
+print (" ")
+
+nama          = input ('Masukkan Nama Anda              :  ')
+alamat        = input ('Masukkan Alamat Anda            :  ')
+tempat_lahir  = input ('Masukkan Tempat Lahir Anda      :  ')
+tanggal_lahir = input ('Masukkan Tanggal Lahir Anda     :  ')
+hobi          = input ('Masukkan Hoby Anda              :  ')
+
+
+print (" ")
+print (" ")
+
+print ("Nama Anda Adalah                : ",nama )
+print ("Alamat Anda Adalah              : ",alamat)
+print ("Tempat Lahir Anda Adalah        : ",tempat_lahir)
+print ("Tanggal Lahir Anda adalah       : ",tanggal_lahir)
+print ("Hoby Anda Adalah                : ",hobi)
+
+print (" ")
